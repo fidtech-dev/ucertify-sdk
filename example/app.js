a03935c8886a7c2e79662359669e9b61ceac9ea0
Maximiliano Arias <ariasmaxi89@gmail.com>
2019-07-15 12:51:05 -0300

    updated for new certifier

--- example/app.js
+++ example/app.js
@@ -2,19 +2,28 @@ const UC = require('../lib/universal-certifier');
 const config = require('./config');
 
 const uc = new UC(config.apiKey, config.secret);
+const pictures = [
+  {
+    name: 'picture_0',
+    link: 'https://cloudflare-ipfs.com/ipfs/QmUwHMFY9GSiKgjqyZpgAv2LhBrh7GV8rtLuagbry9wmMU',
+  },
+  {
+    name: 'picture_1',
+    link: 'https://ipfs.io/ipfs/Qmep61aZqJhhmSkhQHUSUme5RFbi8ZfccxXC1TyjKHcEig',
+  },
+];
+
 uc.certify(
-  './example/testfile.jpg',
+  pictures,
   true,
   (res, err) => {
     if (err) throw err;
-    console.log('res');
-    console.log(res);
-
     // Getting the posted certification back
     if (res) {
+      console.log('Getting back the certification ');
+      console.log(res.response.certificationId)
       uc.getCertification(
-        res.id, (innerRes) => {
-          console.log('Getting back the certification ');
+        res.response.certificationId, (innerRes) => {
           console.log(innerRes);
         },
         (innerError) => {
@@ -24,4 +33,3 @@ uc.certify(
     }
   },
 );
-
